d34c50d8e66f7a65a3bfd30800cbd012ad3da25b
SurajPrajapatiDev <122899485+SurajPrajapatiEXT@users.noreply.github.com>
2026-04-03 18:26:14 +0530

    test

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,6 +8,7 @@ const PlaceholderIndex = () => {
     <div className="flex min-h-screen items-center justify-center" style={{ backgroundColor: '#fcfbf8' }}>
       <img data-lovable-blank-page-placeholder="REMOVE_THIS" src="/placeholder.svg" alt="Your app will live here!" />
     </div>
+    <div>Test</div>
   );
 };
 
